711adcc921c5a6fdf2cc6a2cbfa7044136cb7b3d
Malith Kuruppu <info.mkappworks@gmail.com>
2024-08-22 19:21:56 +0200

    Added config server

---
+++ config-server/src/test/java/com/mkappworks/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkappworks.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
