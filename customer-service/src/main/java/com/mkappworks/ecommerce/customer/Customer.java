3fbee18d8cff85b8814de1d904ca14b6013f5dbe
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-17 17:31:18 +0200

    Removed @RequiredArgsConstructor from Customer

--- customer-service/src/main/java/com/mkappworks/ecommerce/customer/Customer.java
+++ customer-service/src/main/java/com/mkappworks/ecommerce/customer/Customer.java
@@ -2,12 +2,10 @@ package com.mkappworks.ecommerce.customer;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@RequiredArgsConstructor
 @Builder
 @Document
 public class Customer {
