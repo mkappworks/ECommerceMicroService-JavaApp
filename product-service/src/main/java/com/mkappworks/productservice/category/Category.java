c4c1f31038c2ab9480f50ffef609722c7abbc39a
Malith Kuruppu <info.mkappworks@gmail.com>
2024-10-19 17:17:03 +0200

    Added product and category models

---
+++ product-service/src/main/java/com/mkappworks/productservice/category/Category.java
@@ -0,0 +1,26 @@
+package com.mkappworks.productservice.category;
+
+import com.mkappworks.productservice.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
