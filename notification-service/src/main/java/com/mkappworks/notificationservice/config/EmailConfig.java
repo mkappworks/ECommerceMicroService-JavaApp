6d3863f729754b67c3e9a36abce063e34055e0fb
Malith Kuruppu <info.mkappworks@gmail.com>
2024-10-26 22:13:41 +0200

    Added notification-service EmailConfig

---
+++ notification-service/src/main/java/com/mkappworks/notificationservice/config/EmailConfig.java
@@ -0,0 +1,15 @@
+package com.mkappworks.notificationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
